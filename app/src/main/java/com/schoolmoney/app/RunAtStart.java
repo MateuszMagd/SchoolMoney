15c70540e1ddb29f45c5b9e62d492a347a4029fc
MateuszMagd <240742@edu.p.lodz.pl>
2024-12-03 10:33:02 +0100

    Control commit

--- app/src/main/java/com/schoolmoney/app/RunAtStart.java
+++ app/src/main/java/com/schoolmoney/app/RunAtStart.java
@@ -1,20 +1,32 @@
 package com.schoolmoney.app;
 
+import com.schoolmoney.app.repository.UserRepository;
+import com.schoolmoney.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RunAtStart {
-
+    private final UserRepository userRepository;
     @Autowired
-    public RunAtStart() {
+    public RunAtStart(UserRepository userRepository) {
         super();
 
         //
         // ! BELOW CONNECT REPOSITORIES/SERVICIES !
         //
 
+        this.userRepository = userRepository;
     }
 
-
+    @Autowired
+    @Bean
+    public CommandLineRunner runner(UserRepository userRepository)
+    {
+        return args -> {
+            System.out.println("APPLICATION STARTED!");
+        };
+    }
 }
