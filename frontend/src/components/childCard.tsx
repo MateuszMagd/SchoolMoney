a176c30d99228468269f37a595698cb2a4cb0076
kaszczu02 <240701@edu.p.lodz.pl>
2025-02-10 18:03:50 +0100

    Fix parent page

--- frontend/src/components/childCard.tsx
+++ frontend/src/components/childCard.tsx
@@ -6,30 +6,30 @@ const ChildCard = ({childInfo}: {childInfo: ChildInfo}) => {
     const { sessionId, firstName, lastName, photo, pesel, birthday } = childInfo;
     console.log(childInfo)
     return (
-        <div className="flex flex-col mx-auto bg-white rounded-[30px] shadow-lg"style={{ height: '750px', width: '585px' }}>
+        <div className="flex flex-col mx-auto bg-white rounded-[30px] shadow-lg"style={{ height: '710px', width: '580px' }}>
 
-                <div className="flex justify-center items-center h-[200px] mt-7">
-                    <Image src={photo} alt="photo" width={168} height={155} />
+                <div className="flex justify-center items-center h-[200px] mt-3">
+                    <Image src={photo} alt="photo" width={158} height={155} />
                 </div>
 
-                <div className="flex justify-center items-center space-x-2">
+                <div className="flex justify-center items-center space-x-2 mt-5">
                     <div className="font-anton text-normal_blue" style={{ fontSize: '32px' }}>{firstName}</div>
                     <div className="font-anton text-normal_blue" style={{ fontSize: '32px' }}>{lastName}</div>
                 </div>
 
-                <div className="flex justify-center font-anton text-normal_blue mt-5" style={{ fontSize: '22px' }}>PESEL</div>
+                <div className="flex justify-center font-anton text-normal_blue mt-8" style={{ fontSize: '23px' }}>PESEL</div>
                 <div className="flex justify-center font-oswald text-normal_blue" style={{ fontSize: '20px' }}>{pesel}</div>
 
-                <div className="flex justify-center font-anton text-normal_blue mt-5" style={{ fontSize: '22px' }}>Birthday</div>
+                <div className="flex justify-center font-anton text-normal_blue mt-6" style={{ fontSize: '23px' }}>Data urodzenia</div>
                 <div className="flex justify-center font-oswald text-normal_blue" style={{ fontSize: '20px' }}>{birthday}</div>
 
                 {/* TODO: Add functionality after class module done! */}
-                <div className="flex justify-center font-anton text-normal_blue mt-5" style={{ fontSize: '22px' }}>Class</div>
-                <div className="flex justify-center font-oswald text-normal_blue" style={{ fontSize: '20px' }}>1D</div>
+                <div className="flex justify-center font-anton text-normal_blue mt-6" style={{ fontSize: '23px' }}>Grupa</div>
+                <div className="flex justify-center font-oswald text-normal_blue" style={{ fontSize: '22px' }}>1D</div>
 
                 {/* TODO: Add functionality after class module done! */}
-                <div className="flex justify-center font-anton text-normal_blue mt-5" style={{ fontSize: '22px' }}>Patron</div>
-                <div className="flex justify-center font-oswald text-normal_blue" style={{ fontSize: '20px' }}>Edyta Nowak</div>
+                <div className="flex justify-center font-anton text-normal_blue mt-6" style={{ fontSize: '23px' }}>Opiekun</div>
+                <div className="flex justify-center font-oswald text-normal_blue mb-3" style={{ fontSize: '22px' }}>Edyta Nowak</div>
                
                 <div className="flex justify-center items-center mt-5 mb-5">
                     <RouterButton page={`edit/child/${sessionId}`} buttonString="Edytuj" color="bg-dark_blue" width="w-[150px]" height="h-[50px]"/>
--- frontend/src/components/parentCard.tsx
+++ frontend/src/components/parentCard.tsx
@@ -6,13 +6,13 @@ const ParentCard = ({ info }: { info: UserInfo }) => {
     const { email, firstName, lastName, photo, pesel, userType } = info;
 
     return (
-        <div className="flex flex-col mx-auto bg-white rounded-[30px] shadow-lg" style={{ height: '815px', width: '550px' }}>
+        <div className="flex flex-col mx-auto bg-white rounded-[30px] shadow-lg" style={{ height: '700px', width: '580px' }}>
            
-            <div className="flex flex-col justify-center items-center h-[200px] mt-7 mb-7">
-                <Image src={photo} alt="photo" width={168} height={155} />
+            <div className="flex flex-col justify-center items-center h-[200px] mt-3">
+                <Image src={photo} alt="photo" width={155} height={155} />
             </div>
 
-            <div className="flex justify-center items-center space-x-2">
+            <div className="flex justify-center items-center space-x-2 ">
                 <div className="font-anton text-normal_blue" style={{ fontSize: '32px' }}>{firstName}</div>
                 <div className="font-anton text-normal_blue" style={{ fontSize: '32px' }}>{lastName}</div>
             </div>
@@ -20,17 +20,18 @@ const ParentCard = ({ info }: { info: UserInfo }) => {
             
 
             <div className="flex justify-center font-anton text-normal_blue" style={{ fontSize: '22px' }}>{userType}</div>
-            <div className="flex justify-center font-anton text-normal_blue mt-10" style={{ fontSize: '22px' }}>EMAIL</div>
-            <div className="flex justify-center font-oswald text-normal_blue" style={{ fontSize: '20px' }}>{email}</div>
-            <div className="flex justify-center font-anton text-normal_blue mt-10" style={{ fontSize: '22px' }}>PESEL</div>
+            <div className="flex justify-center font-anton text-normal_blue mt-10" style={{ fontSize: '23px' }}>EMAIL</div>
+            <div className="flex justify-center font-oswald text-normal_blue" style={{ fontSize: '22px' }}>{email}</div>
+            <div className="flex justify-center font-anton text-normal_blue mt-6" style={{ fontSize: '23px' }}>PESEL</div>
             <div className="flex justify-center font-oswald text-normal_blue" style={{ fontSize: '20px' }}>{pesel}</div>
-            <div className="flex justify-center font-anton text-normal_blue mt-10" style={{ fontSize: '22px' }}>Numer rachunku</div>
+            <div className="flex justify-center font-anton text-normal_blue mt-6" style={{ fontSize: '23px' }}>Numer rachunku</div>
             {/* TODO: Change it to database value */}
             <div className="flex justify-center font-oswald text-normal_blue mb-10" style={{ fontSize: '20px' }}>01 2345 6789 1011 1213 1415 1617</div>
-            <div className="flex flex-col items-center justify-center space-y-4 mb-10">
+
+            <div className="flex flex-row items-center justify-center space-x-12">
                 {/* TODO: Add functionality to this */}
-                <RouterButton page="/parent-details" buttonString="Edytuj" width="w-[150px]" height="h-[50px]"/>
-                <RouterButton page="/parent-details" buttonString="Raporty" width="w-[150px]" height="h-[50px]"/>
+                <RouterButton page="/parent-details" buttonString="Edytuj"  color="bg-dark_blue" width="w-[150px]" height="h-[50px] "/>
+                <RouterButton page="/parent-details" buttonString="Raporty"  color="bg-dark_blue" width="w-[150px]" height="h-[50px]"/>
             </div>
             
             
