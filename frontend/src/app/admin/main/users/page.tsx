edcb37e8c90d870707a16943de2f41a5f8fcf83f
kaszczu02 <240701@edu.p.lodz.pl>
2025-02-15 21:31:14 +0100

    Add graphics to users in admin panel

--- frontend/src/app/admin/main/users/page.tsx
+++ frontend/src/app/admin/main/users/page.tsx
@@ -2,7 +2,11 @@ import UsersPage from "@/components/edits-components/userTable";
 
 const Page = () => {
     return (
-        <div className="flex flex-row justify-center text-2xl mt-10">
+        <div className="flex flex-row justify-center text-2xl  min-h-screen"
+         style={{
+                backgroundImage: "url('/assets/admin_background.png')",
+                backgroundSize: "cover",
+            }}>
              <UsersPage />
         </div>
     );
--- frontend/src/components/edits-components/userInfoRow.tsx
+++ frontend/src/components/edits-components/userInfoRow.tsx
@@ -9,25 +9,25 @@ interface UserInfoRowProps {
 
 const UserInfoRow: React.FC<UserInfoRowProps> = ({ user, onDelete, onModify }) => {
     return (
-        <tr className="text-center">
+        <tr className="text-center ">
             <td className="p-4">{user.firstName}</td>
             <td className="p-4">{user.lastName}</td>
             <td className="p-4">{user.email}</td>
             <td className="p-4">{user.pesel}</td>
             <td className="p-4">{user.userType}</td>
-            <td className="flex flex-row justify-center"><Image src={user.photo} alt="No photo" width={100} height={100} className=""/></td>
+            <td className="flex flex-row justify-center"><Image src={user.photo} alt="No photo" width={70} height={70} className=""/></td>
             <td>
                 <button 
                     onClick={onModify} 
-                    className="bg-blue-500 text-white p-1 mx-1"
+                    className="bg-light_blue text-white font-bold w-[100px] h-[50px] rounded-lg text-[13px] mr-3"
                 >
-                    Modify
+                    MODYFIKUJ
                 </button>
                 <button 
                     onClick={onDelete} 
-                    className="bg-red-500 text-white p-1 mx-1"
+                    className="bg-light_marine text-white font-bold w-[100px] h-[50px] rounded-lg text-[13px]"
                 >
-                    Delete
+                    USUŃ
                 </button>
             </td>
         </tr>
