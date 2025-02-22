77d48feea0dc1914a09b221b631b7ee9bd074860
kaszczu02 <240701@edu.p.lodz.pl>
2025-02-23 00:56:11 +0100

    Add graphics to transactions

--- frontend/src/app/transactions/page.tsx
+++ frontend/src/app/transactions/page.tsx
@@ -69,24 +69,34 @@ const TransactionPage = () => {
     };
 
     return (
-        <div className="flex flex-col items-center space-y-4">
-            <h1>Transactions</h1>
+        <div className="flex flex-col justify-center items-center min-h-screen space-y-14"   
+            style={{
+            backgroundImage: "url('/assets/admin_background.png')",
+            backgroundSize: "cover",}}>
 
-            <form>
-                <select onChange={(e) => onSelectChange(e)}>
-                    <option value="">Select transaction</option>
+        <div className="p-5 bg-white rounded-[30px] w-[580px] h-[730px] shadow-xl justify-center items-center">
+        <h3 className="text-[55px] font-anton text-dark_blue mt-5 mb-12 text-center">TRANSAKCJE</h3>
+            
+        <div className="flex justify-center items-center"> 
+            <form> 
+                <select 
+                    onChange={(e) => onSelectChange(e)}
+                    className="w-[500px] items-center p-2 border border-gray-300 font-[Open_Sans] rounded-md focus:border-dark_blue focus:outline-none text-dark_blue" 
+                >
+                    <option value="" className="text-gray-500">Wybierz osobę / zbiórkę</option>
                     {transactionData.map((transaction, index) => (
-                        <option key={index} value={transaction.name}>
+                        <option key={index} value={transaction.name} className="text-dark_blue">
                             {transaction.name}
                         </option>
                     ))}
                 </select>
             </form>
+            </div>
 
             {selectedTransaction ?  (
                 <div className="flex flex-col items-center space-y-4">
-                    <h2>Detale:</h2>
-                    <p>Imie/Nazwa Zbiórki: {selectedTransaction.name}</p>
+                    {/* <h2>Detale:</h2> */}
+                    {/* <p>Imie/Nazwa Zbiórki: {selectedTransaction.name}</p> */}
                     {selectedTransaction.amountNeeded != 0 ? (
                          <p>Pieniądze: {selectedTransaction.amountNeeded}</p>
                     ) :
@@ -100,27 +110,35 @@ const TransactionPage = () => {
                     ) :
                     (<></>)}
                     <form onSubmit={() => onSubmit()} className="flex flex-col items-center space-y-4">
-                        <input type="number" id="amount" placeholder="Ile chcesz przelać?" onChange={(e) => onChange(e)}></input>
-                        <textarea id="text" placeholder="Opis" onChange={(e) => setText(e.target.value)}></textarea>
-                        <button className="p-5 bg-blue-600 text-white" type="submit">Transakcja</button>
+                        <input type="number" id="amount" placeholder="Podaj kwotę" onChange={(e) => onChange(e)}
+                        className="w-[400px] mt-10 p-3 border border-gray rounded-md focus:outline-none focus:border-dark_blue placeholder-dark_blue resize-none">
+                        </input>
+                        <textarea id="text" placeholder="Opis" onChange={(e) => setText(e.target.value)}
+                        className="w-[400px] h-[120px] mt-10 p-3 border border-gray rounded-md focus:outline-none focus:border-dark_blue placeholder-dark_blue resize-none">
+                        </textarea>
+                       
                     </form>   
                 </div>
             ) : (
                 <div>
-                    <h2>Transaction details</h2>
-                    <p>No transaction selected</p>
+                    {/* <h2>Transaction details</h2> */}
+                    {/* <p>No transaction selected</p> */}
                 </div>
             )}
 
-
+            <div className="flex flex-col justify-center items-center mt-10 space-y-5"  >
+            
+            <button className="w-[280px] h-[60px] p-5 bg-dark_blue text-white text-center rounded-lg hover:opacity-90 transition" type="submit">Zrób przelew</button>
             <RouterButton
                 page="transactions/my-all"
-                buttonString="Zobacz moje wszystkie transakcje"
-                color="bg-dark_blue"
-                width="w-[150px]"
+                buttonString="Pokaż płatności"
+                color="bg-light_blue"
+                width="w-[280px]"
                 height="h-[60px]"/>
+            </div>
+             </div>
 
-            <RouterButton page="/" buttonString="Wróć do głównej" color="bg-dark_blue" width="w-[150px]" height="h-[60px]"/>
+             <RouterButton page="/" buttonString="Wróć do głównej" color="bg-marine" width="w-[250px]" height="h-[60px]"/>
         </div>
     );
 };
