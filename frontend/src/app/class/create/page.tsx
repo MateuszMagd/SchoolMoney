5f0571f1100ce2116573f90373fab388882be6d0
kaszczu02 <240701@edu.p.lodz.pl>
2025-02-23 21:46:54 +0100

    Add graphics to class list

--- frontend/src/app/class/create/page.tsx
+++ frontend/src/app/class/create/page.tsx
@@ -15,13 +15,25 @@ const CreatePage = () => {
     };
 
     return (
-        <div className="flex flex-col content-center p-10 space-y-6">
-            <h1 className="text-center">Create Page</h1>
-            <form className="flex flex-col content-center space-y-6" onSubmit={handleSubmit}>
-                <label htmlFor="class-name">Class Name</label>
-                <input type="text" id="classname" name="classname" />
-                <button type="submit">Create</button>
+        <div className="flex flex-col justify-center items-center min-h-screen space-y-14"
+        style={{
+            backgroundImage: "url('/assets/class_edit_background.png')",
+            backgroundSize: "cover",
+        }}>
+         <div className="p-5 bg-white rounded-[30px] w-[580px] h-[730px] shadow-2xl justify-center items-center">
+           <div className="font-anton text-dark_blue mt-[100px] mb-5 text-center" style={{ fontSize: '55px' }}>KREATOR KLAS</div>
+           <label htmlFor="class-name" className="block text-[17px] font-medium text-dark_blue font-[Open_Sans] ml-6 mt-20">Nazwa klasy:</label>
+            <form className="flex flex-col items-center space-y-2" onSubmit={handleSubmit}>
+    
+                <input type="text" id="classname" name="classname" placeholder="Podaj nazwę nowej klasy"
+                className="w-[93%] p-2 border border-gray-300 font-[Open_Sans] rounded-md focus:border-dark_blue focus:outline-none text-dark_blue"></input>
+                
+                <div className="flex justify-center">
+                    <button type="submit" className="w-[200px] h-[50px] bg-dark_blue text-white rounded-md mb-12 mt-12">Zapisz</button>
+                </div>
+               
             </form>
+        </div>
         </div>
     );
 };
--- frontend/src/app/class/manage/page.tsx
+++ frontend/src/app/class/manage/page.tsx
@@ -19,23 +19,35 @@ const ManagePage = () => {
 
 
     return (
-        <div className="flex flex-col items-center h-screen">
-            <h1>Your classes</h1>
-            <table>
-                <thead>
+        <div className="flex flex-col min-h-screen space-y-14" 
+            style={{
+                backgroundImage: "url('/assets/class_edit_background.png')",
+                backgroundSize: "cover",}}>
+
+        <div className="container mx-auto bg-white rounded-[30px] shadow-2xl w-[580px] h-[530px] p-6 mt-20">
+            <h3 className="text-[55px] font-anton text-dark_blue mt-5 mb-4 text-center">TWOJE KLASY</h3>
+            <table className="table-auto w-full space-x-5 font-[Open_Sans] text-[18px] text-dark_blue mt-10">
+                <thead className="text-[20px] font-bold ">
                     <tr>
-                        <th>Nazwa Klasy</th>
+                        <th className="pb-4">NAZWA KLASY</th>
                     </tr>
                 </thead>
                 <tbody>
                     {classes.map((classData) => (
-                        <tr key={classData.sessionId}>
+                        <tr className="text-center pt-4" key={classData.sessionId}>
                             <td>{classData.className}</td>
-                            <td><RouterButton page={`class/manage/${classData.sessionId}`}buttonString="Manage"/></td>
+                            <div className="flex justify-center items-center text-[15px]">
+                                <td><RouterButton page={`class/manage/${classData.sessionId}`}buttonString="Zarządzaj" 
+                                color="bg-dark_blue"
+                                width="w-[180px]"
+                                height="h-[40px]"/></td>
+                            </div>
+                         
                         </tr>
                     ))}
                 </tbody>
             </table>
+        </div>
         </div>
     );
 };
