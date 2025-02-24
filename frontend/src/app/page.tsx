39635a59e4c44332d89bc5d01c87ebaa1943ee32
kaszczu02 <240701@edu.p.lodz.pl>
2025-02-24 21:22:31 +0100

    Add graphics to menu page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -53,19 +53,41 @@ export default function Home() {
   return (
     <div 
         className="min-h-screen flex justify-center items-center bg-cover bg-center"
-        style={{ backgroundImage: "url('/assets/main_page_background.png')" }} 
+        style={{ backgroundImage: "url('/assets/main_page_background_2.png')" }} 
     >
       <main className="flex flex-col p-20 mt-20 space-y-6">
         
         {isLogged() ? (
           <>
-            <button onClick={logout}>Logout!</button> 
-            <RouterButton page="parent-details" buttonString="Konto rodzica" />
-            <RouterButton page="fund-page" buttonString="Zbiórka" />
-            <RouterButton page="transactions" buttonString="Przelewy" />
-            <RouterButton page="class" buttonString="Klasa" />
-            <RouterButton page="chat" buttonString="Chat" />
-            <button onClick={test}>Show me if logged!</button>
+            <div className="fixed top-0 left-0 w-full bg-white shadow-md p-4 flex items-center justify-between">
+            <h1 className="text-dark_blue text-[35px] font-anton ml-6">ZBIÓRKI KLASOWE</h1> 
+            <button onClick={logout} 
+                className="w-[100px] h-[40px] text-dark_blue bg-transparent font-anton text-[22px] mr-5">
+                WYLOGUJ
+            </button>
+
+            </div>
+          
+          <div className="flex flex-col space-y-8 justify-center b-20 relative top-[-80px]">
+
+          <div className="flex flex-row space-x-8 mr-6 font-anton text-[22px]">
+            <RouterButton page="parent-details" buttonString="Konto Rodzica" color="bg-dark_blue" width="w-[200px]" height="h-[100px]"/>
+            <RouterButton page="fund-page" buttonString="Zbiórka" color="bg-normal_blue" width="w-[200px]" height="h-[100px]"/>
+          </div> 
+
+          <div className="flex flex-row space-x-8 mr-6 font-anton text-[22px]">
+            <RouterButton page="transactions" buttonString="Przelewy" color="bg-light_blue" width="w-[200px]" height="h-[100px]"/>
+            <RouterButton page="class" buttonString="Klasa" color="bg-marine" width="w-[200px]" height="h-[100px]"/>
+          </div>
+
+          <div className="flex flex-row mr-[25px] font-anton text-[22px] justify-center">
+            <RouterButton page="chat" buttonString="Chat" color="bg-light_marine" width="w-[200px]" height="h-[100px]"/>
+          </div>
+          
+          </div>
+          
+            {/* <button onClick={test}>Show me if logged!</button> */}
+         
           </>
         ) : (
           <div className="fixed top-0 left-0 w-full bg-white shadow-md p-4 flex items-center justify-between">
