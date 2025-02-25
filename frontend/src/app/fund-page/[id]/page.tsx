56a8ea153d055040c3ef8846f8761cd546fc24cc
kaszczu02 <240701@edu.p.lodz.pl>
2025-02-25 20:09:30 +0100

    Add graphics to single fund preview

--- frontend/src/app/fund-page/[id]/page.tsx
+++ frontend/src/app/fund-page/[id]/page.tsx
@@ -27,14 +27,62 @@ const showFundPage = () => {
 
     return (
         <div>
-            <div>Zbiórka: {fund.name}</div>
-            <div>{fund.money}</div>
-            {/*"TODO: Add date"*/}
-            <div>10-10-2024</div>
-            <div>10-10-2025</div>
-            <div>{fund.description}</div>
-            <div>{fund.classSessionId}</div>
-            <div>{fund.statusType}</div>
+             <div className="flex flex-col min-h-screen w-full"
+            style={{
+                backgroundImage: "url('/assets/fund_background.png')",
+                backgroundSize: "cover",
+            }}>
+
+            <div className=" items-start  font-anton text-marine mt-6 ml-[70px] mb-5" style={{ fontSize: '45px' }}>{fund.name}</div>
+            <div className="flex w-full">
+                <div className="w-1/2 p-4 ml-10">
+                <div className="flex flex-col items-start">
+               
+                 {/*"TODO: Add date"*/}
+                <div className="flex flex-row items-start gap-x-10">
+                    <div className="flex flex-col items-start">
+                        <p className=" ml-3 text-dark_blue font-[Open_Sans] font-bold">DATA ROZPOCZĘCIA:</p>
+                        <div className="w-[200px] border border-dark_blue rounded-md p-2 m-2 placeholder-dark_blue focus:border-marine focus:outline-none text-dark_blue">10-10-2024</div>
+                    </div>
+
+                    <div className="flex flex-col items-start">
+                        <p className=" ml-3 text-dark_blue font-[Open_Sans] font-bold">DATA ZAKOŃCZENIA:</p>
+                        <div className="w-[200px] border border-dark_blue rounded-md p-2 m-2 placeholder-dark_blue focus:border-marine focus:outline-none text-dark_blue">10-10-2025</div>
+                       
+                    </div>
+                </div>
+
+                <div className="flex flex-row items-start gap-x-10">
+                <div className="flex flex-col items-start">
+                    <p className=" ml-3 text-dark_blue font-[Open_Sans] font-bold">KLASA:</p>
+                    <div className="w-[200px] border border-dark_blue rounded-md p-2 m-2 placeholder-dark_blue focus:border-marine focus:outline-none text-dark_blue">{fund.classSessionId}</div>
+                </div>
+
+                <div className="flex flex-col items-start">
+                    <p className=" ml-3 text-dark_blue font-[Open_Sans] font-bold">STATUS:</p>
+                    <div className="w-[200px] border border-dark_blue rounded-md p-2 m-2 placeholder-dark_blue focus:border-marine focus:outline-none text-dark_blue">{fund.statusType}</div>
+                </div>
+
+                </div>
+            
+                
+                <div className="flex flex-col items-start">
+                    <p className=" ml-3 text-dark_blue font-[Open_Sans] font-bold">KWOTA:</p>
+                    <div className="w-[200px] border border-dark_blue rounded-md p-2 m-2 placeholder-dark_blue focus:border-marine focus:outline-none text-dark_blue">{fund.money}zł</div>
+                </div>
+
+                <div className="flex flex-col items-start">
+                    <p className=" ml-3 text-dark_blue font-[Open_Sans] font-bold">OPIS ZBIÓRKI:</p>
+                    <div className="w-[720px] h-[200px] border border-dark_blue rounded-md p-2 m-2 placeholder-dark_blue focus:border-marine focus:outline-none text-dark_blue">{fund.description}</div>
+                </div>
+            </div>
+                </div>
+                <div className="ml-32">
+                    <img src="/assets/hand_with_money.png"  className="" />
+                </div>
+            </div>   
+            
+        </div>
         </div>
     )
 }
